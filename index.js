31fcf08e8bfd0da8f102c231b916becb1c325b98
Chhand Mehta <Chhand@Chhands-MacBook-Pro.local>
2021-03-06 01:18:49 -0500

    Functional code for sample server application

--- index.js
+++ index.js
@@ -3,34 +3,86 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'), 
-            (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
+    // if (req.url === '/') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'index.html'), 
+    //         (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.end(content);
+    //         }
+    //     );
+    // }
+    // if (req.url === '/about') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'about.html'), 
+    //         (err, content) => {
+    //         if (err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'});
+    //         res.end(content);
+    //         }
+    //     );
+    // }
+    // if (req.url === '/rest/api') {
+    //     const users = [
+    //         {'name': 'Bob Smith', 'age': 36},
+    //         {'name': 'John Doe', 'age': 45}
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     res.end(JSON.stringify(users));
+    // }
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    console.log(filePath);
+
+    // Extension of file
+    let extName = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set contentType 
+    switch (extName) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code = 'ENOET') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type' : 'text/html' });
+                    res.end(content, 'utf8');
+                })
             }
-        );
-    }
-    if (req.url === '/about') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'about.html'), 
-            (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
+            else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error ${err.code}`);
             }
-        );
-    }
-    if (req.url === '/rest/api') {
-        const users = [
-            {'name': 'Bob Smith', 'age': 36},
-            {'name': 'John Doe', 'age': 45}
-        ];
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(users));
-    }
+        }
+        else {
+            // Success
+            res.writeHead(200, { 'Content-Type' : contentType });
+            res.end(content, 'utf8');
+        }
+    });
 }
 );
 
